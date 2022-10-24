b0e5f5217096271f91d9ebd696ee0b7049d6509e
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-10-25 05:39:54 +0600

    Category Api created

--- index.js
+++ index.js
@@ -5,20 +5,36 @@ const cors = require('cors')
 
 app.use(cors())
 
-const categories = require('./data/category.json')
+const courses = require('./data/courses.json')
+const categories = require('./data/categories.json')
 
 app.get('/', (req, res) => {
     res.send('Learning Platform is Running.')
 })
 
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const course = courses.find(course => course.id === parseInt(id));
+    res.send(course)
+})
+
 app.get('/courses-categories', (req, res) => {
     res.send(categories)
-})
+});
 
 app.get('/courses-categories/:id', (req, res) => {
     const id = req.params.id;
-    const course = categories.find(course => course.id === parseInt(id));
-    res.send(course)
+    if (id === "4") {
+        res.send(courses);
+    }
+    else {
+        const category_course = courses.filter(c => c.category_id === parseInt(id));
+        res.send(category_course);
+    }
 })
 
 
