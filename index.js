0b8eb70815442d1e8434f86c080d71a610001329
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-10-25 01:58:48 +0600

    courses-categories api create

--- index.js
+++ index.js
@@ -5,9 +5,22 @@ const cors = require('cors')
 
 app.use(cors())
 
+const categories = require('./data/category.json')
+
 app.get('/', (req, res) => {
-    res.send('Learning Platform is Running')
+    res.send('Learning Platform is Running.')
 })
+
+app.get('/courses-categories', (req, res) => {
+    res.send(categories)
+})
+
+app.get('/courses-categories/:id', (req, res) => {
+    const id = req.params.id;
+    const course = categories.find(course => course.id === parseInt(id));
+    res.send(course)
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
